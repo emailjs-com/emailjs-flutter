880b483f5d1d804e26fec7f710ea064207de6fc7
Sergey Khomushin <sergey@placer.io>
2022-10-29 22:25:31 +0300

    add http.Client prop for testing

--- lib/api/send_json.dart
+++ lib/api/send_json.dart
@@ -1,17 +1,21 @@
 import 'package:http/http.dart' as http;
 
-Future<String> sendJSON(Uri uri, String data) async {
-  var response = await http.post(
-    uri,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: data,
-  );
+Future<String> sendJSON(http.Client client, Uri uri, String data) async {
+  try {
+    var response = await client.post(
+      uri,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    );
 
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    throw response.body;
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw response.body;
+    }
+  } finally {
+    client.close();
   }
 }
--- lib/emailjs.dart
+++ lib/emailjs.dart
@@ -1,6 +1,7 @@
 library emailjs;
 
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:emailjs/utils/validate_params.dart';
 import 'package:emailjs/api/send_json.dart';
@@ -8,10 +9,12 @@ import 'package:emailjs/api/send_json.dart';
 class EmailJS {
   static String _publicKey = '';
   static String _origin = 'api.emailjs.com';
+  static http.Client _httpClient = http.Client();
 
-  static void init(String publicKey, [String? origin]) {
+  static void init(String publicKey, [String? origin, http.Client? httpClient]) {
     EmailJS._publicKey = publicKey;
     EmailJS._origin = origin ?? 'api.emailjs.com';
+    EmailJS._httpClient = httpClient ?? http.Client();
   }
 
   static Future<String> send(
@@ -33,6 +36,7 @@ class EmailJS {
     };
 
     return await sendJSON(
+      EmailJS._httpClient,
       Uri.https(EmailJS._origin, 'api/v1.0/email/send'),
       json.encode(params),
     );
