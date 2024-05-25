067e266c3e44943b593f5a809d7669e6f5721128
Sergey Khomushin <xr0master@gmail.com>
2024-05-25 18:45:42 +0800

    set version 4

--- lib/emailjs.dart
+++ lib/emailjs.dart
@@ -54,7 +54,7 @@ class EmailJS {
     validateParams(pubKey, serviceID, templateID);
 
     final Map<String, dynamic> params = {
-      'lib_version': '1.3.0',
+      'lib_version': '4.0.0',
       'user_id': pubKey,
       'accessToken': prKey,
       'service_id': serviceID,
