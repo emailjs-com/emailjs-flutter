3116c4bb3926aba0a09670ef90243ccd9b352c5b
Sergey Khomushin <xr0master@gmail.com>
2024-06-10 01:19:11 +0800

    send: fix typo

--- lib/src/methods/send.dart
+++ lib/src/methods/send.dart
@@ -1,6 +1,10 @@
-import '../models/options.dart';
+import 'dart:convert';
+
 import '../store/store.dart';
 import '../api/send_post.dart';
+
+import '../models/emailjs_response_status.dart';
+import '../models/options.dart';
 
 import '../utils/validate_params.dart';
 import '../utils/is_blocked_value_in_params.dart';
@@ -12,19 +16,19 @@ import '../errors/limit_rate_error.dart';
 
 /// Sends the email through the [serviceID] using the ready-made [templateID].
 ///
-/// It's possible to pass [templatePrams] dynamic variables,
+/// It's possible to pass [templateParams] dynamic variables,
 /// and set the [options] for this call.
 Future<EmailJSResponseStatus> send(
   String serviceID,
   String templateID, [
-  Map<String, dynamic>? templatePrams,
+  Map<String, dynamic>? templateParams,
   Options? options,
 ]) async {
   final publicKey = options?.publicKey ?? store.publicKey;
   final privateKey = options?.privateKey ?? store.privateKey;
   final storageProvider = options?.storageProvider ?? store.storageProvider ?? createDefaultStorage();
-  final blockList = { ...store.blockList, ...options.blockList };
-  final limitRate = { ...store.limitRate, ...options.limitRate };
+  final blockList = { ...store.blockList, ...options?.blockList };
+  final limitRate = { ...store.limitRate, ...options?.limitRate };
 
   validateParams(publicKey, serviceID, templateID);
 
@@ -42,7 +46,7 @@ Future<EmailJSResponseStatus> send(
     'accessToken': privateKey,
     'service_id': serviceID,
     'template_id': templateID,
-    'template_params': templatePrams,
+    'template_params': templateParams,
   };
 
   return sendPost('api/v1.0/email/send', json.encode(params));
