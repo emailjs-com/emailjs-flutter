c5cf9b49ee511f4e044c53c56e5a70700053af4c
Sergey Khomushin <xr0master@gmail.com>
2024-06-10 19:03:36 +0800

    the required params can be also null

--- lib/src/utils/validate_params.dart
+++ lib/src/utils/validate_params.dart
@@ -1,18 +1,18 @@
 /// Validates required params
 void validateParams(
-  String publicKey,
-  String serviceID,
-  String templateID,
+  String? publicKey,
+  String? serviceID,
+  String? templateID,
 ) {
-  if (publicKey.isEmpty) {
+  if (publicKey == null || publicKey.isEmpty) {
     throw 'The public key is required. Visit https://dashboard.emailjs.com/admin/account';
   }
 
-  if (serviceID.isEmpty) {
+  if (serviceID == null || serviceID.isEmpty) {
     throw 'The service ID is required. Visit https://dashboard.emailjs.com/admin';
   }
 
-  if (templateID.isEmpty) {
+  if (templateID == null || templateID.isEmpty) {
     throw 'The template ID is required. Visit https://dashboard.emailjs.com/admin/templates';
   }
 }
