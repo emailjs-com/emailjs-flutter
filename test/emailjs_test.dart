8fbfcadf6f4a9d367698c957602137dac9885aab
Sergey Khomushin <xr0master@gmail.com>
2024-06-11 00:12:39 +0800

    fix old tests for new code

--- test/emailjs_test.dart
+++ test/emailjs_test.dart
@@ -8,39 +8,45 @@ class MockClient extends Mock implements http.Client {}
 
 class FakeUri extends Fake implements Uri {}
 
+class MockStorage extends Mock implements StorageProvider {}
+
 void main() {
   setUpAll(() {
     registerFallbackValue(FakeUri());
+
+    init(Options(
+        storageProvider: MockStorage(),
+    ));
   });
 
   group('required params validator', () {
     test('should send method and fail on the public key', () {
       expect(
-        () => EmailJS.send('default_service', 'my_test_template'),
+        () => send('default_service', 'my_test_template'),
         throwsA(startsWith('The public key is required')),
       );
     });
 
     test('should send method and fail on the service ID', () {
-      EmailJS.init(const Options(
+      init(const Options(
         publicKey: 'LC2JWGTestKeySomething',
         privateKey: 'PrKeyTestKeySomething',
       ));
 
       expect(
-        () => EmailJS.send('', 'my_test_template'),
+        () => send('', 'my_test_template'),
         throwsA(startsWith('The service ID is required')),
       );
     });
 
     test('should send method and fail on the template ID', () {
-      EmailJS.init(const Options(
+      init(const Options(
         publicKey: 'LC2JWGTestKeySomething',
         privateKey: 'PrKeyTestKeySomething',
       ));
 
       expect(
-        () => EmailJS.send('default_service', ''),
+        () => send('default_service', ''),
         throwsA(startsWith('The template ID is required')),
       );
     });
@@ -56,17 +62,16 @@ void main() {
             body: any(named: 'body'),
           )).thenAnswer((_) async => http.Response('OK', 200));
 
-      EmailJS.init(
+      init(
         const Options(
           publicKey: 'LC2JWGTestKeySomething',
           privateKey: 'PrKeyTestKeySomething',
         ),
-        null,
         mockHttpClient,
       );
 
       try {
-        final result = await EmailJS.send(
+        final result = await send(
           'default_service',
           'my_test_template',
         );
@@ -87,16 +92,15 @@ void main() {
           )).thenAnswer((_) async => http.Response('OK', 200));
 
       // pass the mock http client
-      EmailJS.init(
+      init(
         const Options(
           publicKey: '',
         ),
-        null,
         mockHttpClient,
       );
 
       try {
-        final result = await EmailJS.send(
+        final result = await send(
           'default_service',
           'my_test_template',
           null,
@@ -121,17 +125,16 @@ void main() {
       )).thenAnswer((_) async => http.Response('The Public Key is required', 403));
 
       // pass the mock http client
-      EmailJS.init(
+      init(
         const Options(
           publicKey: 'LC2JWGTestKeySomething',
           privateKey: 'PrKeyTestKeySomething',
         ),
-        null,
         mockHttpClient,
       );
 
       try {
-        final result = await EmailJS.send(
+        final result = await send(
           'default_service',
           'my_test_template',
         );
