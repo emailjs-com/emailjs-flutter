59e6e1263462d85fa2578ed062ef55439be6ced0
Sergey Khomushin <xr0master@gmail.com>
2024-06-10 19:32:21 +0800

    export sub options

--- lib/emailjs.dart
+++ lib/emailjs.dart
@@ -3,5 +3,7 @@ library emailjs;
 export 'src/models/emailjs_response_status.dart';
 export 'src/models/storage_provider.dart';
 export 'src/models/options.dart';
+export 'src/models/block_list.dart';
+export 'src/models/limit_rate.dart';
 export 'src/methods/init.dart';
 export 'src/methods/send.dart';
