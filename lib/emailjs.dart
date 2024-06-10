edf091adcb64db7c6dec281afa9c5df9c77e5c27
Sergey Khomushin <xr0master@gmail.com>
2024-06-10 19:06:08 +0800

    emailjs: improve export

--- lib/emailjs.dart
+++ lib/emailjs.dart
@@ -1,20 +1,7 @@
 library emailjs;
 
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-import 'src/models/emailjs_response_status.dart';
-import 'src/models/options.dart';
-export 'src/models/block_list.dart';
-export 'src/models/limit_rate.dart';
-import 'src/models/storage_provider.dart';
-import 'src/methods/init.dart';
-import 'src/methods/send.dart';
-
 export 'src/models/emailjs_response_status.dart';
-export 'src/models/options.dart';
-export 'src/models/block_list.dart';
-export 'src/models/limit_rate.dart';
 export 'src/models/storage_provider.dart';
+export 'src/models/options.dart';
 export 'src/methods/init.dart';
 export 'src/methods/send.dart';
