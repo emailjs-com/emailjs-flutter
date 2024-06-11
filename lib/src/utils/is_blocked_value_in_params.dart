f2b1c63045a1ef43220fcca5479a4c24c4963475
Sergey Khomushin <xr0master@gmail.com>
2024-06-11 17:04:37 +0800

    isBLockedValueInParams: fix the data condition

--- lib/src/utils/is_blocked_value_in_params.dart
+++ lib/src/utils/is_blocked_value_in_params.dart
@@ -1,9 +1,15 @@
 import '../models/block_list.dart';
 
+bool isBlockListDisabled(BlockList options) {
+  return options.list == null || options.list!.isEmpty || options.watchVariable == null;
+}
+
 bool isBlockedValueInParams(
   BlockList options,
   Map<String, dynamic> params,
 ) {
+    if (isBlockListDisabled(options)) return false;
+
     final value = params[options.watchVariable];
-    return options.list != null && options.list!.contains(value);
+    return options.list!.contains(value);
 }
